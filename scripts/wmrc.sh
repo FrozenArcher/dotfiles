2759e50ab08fb5bddf9abc901779c518bc66652a
FrozenArcher <yhy242989105@hotmail.com>
2023-07-28 02:52:01 +0800

    add scripts

---
+++ scripts/wmrc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+run() {
+	if ! pgrep -f "$1" ; then
+		"$@"
+	fi
+}
+
+# polkit
+_polkit=/usr/lib/polkit-kde-authentication-agent-1
+if [ -f $_polkit ]; then
+    $_polkit &
+fi
+
+# run wm app
+run picom -b
+run nm-applet &
+run udiskie --tray &
+run blueman-applet &
+run cbatticon &
+run xfce4-power-manager &
+run caffeine &
+run xfce4-screensaver &
+run pasystray &
