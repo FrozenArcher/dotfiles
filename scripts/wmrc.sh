a3e4a2486ede1668959064ba7821996f9570c0a7
FrozenArcher <yhy242989105@hotmail.com>
2024-03-29 00:58:01 +0800

    (wm) add gnome keyring

--- scripts/wmrc.sh
+++ scripts/wmrc.sh
@@ -8,8 +8,12 @@ run() {
 
 # polkit
 _polkit=/usr/lib/polkit-kde-authentication-agent-1
+_polkit_gnome=/usr/bin/gnome-keyring-daemon
 if [ -f $_polkit ]; then
     $_polkit &
+elif [ -f $_polkit_gnome ]; then
+    dbus-update-activation-environment --all
+    gnome-keyring-daemon --start --components=secrets
 fi
 
 # run wm app
